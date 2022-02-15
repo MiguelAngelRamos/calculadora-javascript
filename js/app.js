ad6ff182ee1c0bbc5bff10d2d9808ff7e013e3c3
Miguel Ramos <return.infox@gmail.com>
2022-02-15 20:31:14 -0300

    suma

---
+++ js/app.js
@@ -0,0 +1,90 @@
+let inputValueMemo = 0;
+let operator;
+
+const getContentClick = event => {
+  // console.log(event.target.innerHTML);
+  const value  = event.target.innerHTML;
+  // este value va ser un string "x" o "2"
+  // de que x puede ser transformado a valor numero
+  // en cambio "2" puede ser parseado a 2 number
+  filterAction(value);
+}
+
+// Función para filtrar los procesos
+const filterAction = (value) => {
+  let valorNumber;
+  // HAGO CLICK EN LA X
+  // 4 STRING "4"
+  value != ','?valorNumber = parseInt(value):null;
+  // NaN
+
+  if(isNaN(valorNumber) && value == ',') {
+    addNumberInput(value)
+  } else if(!isNaN(valorNumber) && valorNumber !=','){
+    addNumberInput(parseInt(value));
+  }
+
+  // operaciones
+  value == "+" ? setOperation('+'):null;
+  value == "-" ? setOperation('-'):null;
+  value == "x" ? setOperation('*'):null;
+  value == "/" ? setOperation('/'):null;
+  value == "%" ? setOperation('%'):null;
+  value == "+/-" ? setOperation('+/-'):null;
+}
+
+const addNumberInput = (value) => {
+  
+  //const inputScreen = document.getElementsByClassName('calculator__screen')[0];
+  const inputScreen = document.querySelector('.calculator__screen'); // capturando el elemento
+  const inputValue = inputScreen.value;
+  
+  if(inputValue === "0" && inputValue.length === 1 && value != ',') {
+    inputScreen.value = value
+    return;
+  }
+  console.log(typeof inputValue); // string
+  console.log(typeof value); // numero
+  // no se suman si no que se concatenan
+  inputScreen.value = inputValue + value;
+ 
+}
+
+// operaciones (op)
+const setOperation = (op) => {
+
+  const inputScreenValue = document.querySelector('.calculator__screen').value;
+  // el valor del input
+  operator = op;
+
+  if(inputScreenValue !=0) {
+    // vamos a llamar a otra funcion que nos realize el calculo
+    calculation();
+  }
+}
+
+const calculation = () => {
+  let total = 0;
+  const inputScreen = document.querySelector('.calculator__screen');
+  // <input />
+  let valueOne = inputValueMemo;
+  let valueTwo = parseInt(inputScreen.value);
+
+  console.log('valueOne', typeof valueOne);
+  console.log('valueTwo', typeof valueTwo);
+  console.log(operator);
+  console.log('dentro de calculation');
+
+  // suma
+  if(operator === '+' && inputScreen.value !== "") {
+    console.log('estoy dentro de la suma');
+    total = valueOne + valueTwo;
+  }
+
+  inputValueMemo = total;
+  console.log(inputValueMemo);
+  inputScreen.value = "";
+  inputScreen.placeholder = total;
+  
+
+}
