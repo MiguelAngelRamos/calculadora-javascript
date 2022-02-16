f9c8966e7a140477340932ed9f9f4e87f6192067
Miguel Ramos <return.infox@gmail.com>
2022-02-16 20:04:37 -0300

    operaciones casi completadas

--- js/app.js
+++ js/app.js
@@ -25,14 +25,41 @@ const filterAction = (value) => {
   }
 
   // operaciones
-  value == "+" ? setOperation('+'):null;
-  value == "-" ? setOperation('-'):null;
-  value == "x" ? setOperation('*'):null;
-  value == "/" ? setOperation('/'):null;
-  value == "%" ? setOperation('%'):null;
-  value == "+/-" ? setOperation('+/-'):null;
-
-  value == "=" ? calculation():null;
+  switch (value) {
+    case "+":
+      setOperation("+");
+      break;
+    case "-":
+      setOperation("-");
+      break;
+    case "x":
+      setOperation("*");
+      break;
+    case "/":
+      setOperation("/");
+      break;
+    case "%":
+      setOperation("%");
+      break;
+    case "+/-":
+      setOperation("+/-");
+      break;
+    case "=":
+      calculation();
+      break;
+    case "AC":
+      resetCalculator();
+      break;
+    default:
+      break;
+  }
+  // value == "+" ? setOperation('+'):null;
+  // value == "-" ? setOperation('-'):null;
+  // value == "x" ? setOperation('*'):null;
+  // value == "/" ? setOperation('/'):null;
+  // value == "%" ? setOperation('%'):null;
+  // value == "+/-" ? setOperation('+/-'):null;
+  // value == "=" ? calculation():null;
 }
 
 const addNumberInput = (value) => {
@@ -54,11 +81,10 @@ const addNumberInput = (value) => {
 
 // operaciones (op)
 const setOperation = (op) => {
-
   const inputScreenValue = document.querySelector('.calculator__screen').value;
   // el valor del input
   operator = op;
-
+  console.log(operator);
   if(inputScreenValue !=0) {
     // vamos a llamar a otra funcion que nos realize el calculo
     calculation();
@@ -71,17 +97,60 @@ const calculation = () => {
   // <input />
   let valueOne = transformCommaToPoint(inputValueMemo); // es un number
   let valueTwo = transformCommaToPoint(inputScreen.value); // siempre es un string, pero cuando pasa por la funcion es un numero float
- 
+  
   // suma
   if(operator === '+' && inputScreen.value !== "") {
     total = valueOne + valueTwo;
   }
-
-  inputValueMemo = total;
+ 
+  // La resta
+  if(operator === '-' && inputScreen.value !=="") {
+    if(valueOne !== 0) {
+      total = valueOne - valueTwo;
+    } else {
+      total = valueTwo;
+    }
+  }
   
+  // Multiplicación
+  if(operator === '*' && inputScreen.value !=="") {
+    if(valueOne !== 0) {
+      total = valueOne * valueTwo;
+    } else {
+      total = valueTwo;
+    }
+  }
+
+  // División
+
+  if(operator === '/' && inputScreen.value !=="") {
+    if(valueOne !== 0) {
+      total = valueOne / valueTwo;
+    } else {
+      total = valueTwo;
+    }
+  }
+
+  // Porcentaje
+  if(operator === '%' && inputScreen.value !=="") {
+      total =  valueTwo / 100;
+  }
+  //  "+/-"
+
+  // antes  de asignarle a la variable global de memoria
+  // hacemos la transformacion
+  total = transformPointToComma(total);
+  inputValueMemo = total;
   inputScreen.value = "";
   inputScreen.placeholder = total;
-  
+}
+
+// AC Limpiar el visor
+const resetCalculator = () => {
+  const inputScreen = document.querySelector('.calculator__screen');
+  inputScreen.value = 0;
+  inputValueMemo = 0;
+  operator = null;
 }
 // 5,6 necesitamos trasnformarlo en 5.6
 const transformCommaToPoint = (value) => {
@@ -89,5 +158,12 @@ const transformCommaToPoint = (value) => {
     let resultTransform = value.replace(',','.');
     return parseFloat(resultTransform);
   }
+  // estoy devolviendo un valor numero con coma flotante
   return value;
+}
+
+const transformPointToComma = value => {
+  let resultTransform = value.toString();
+  resultTransform = resultTransform.replace('.', ',');
+  return resultTransform;
 }
